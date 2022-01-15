81c842ccd77d1300eb4e04495c22994ff6e89bae
Nedelcu Victor <victornedelcu90@gmail.com>
2022-01-15 18:16:57 +0200

    last functions

--- main.c
+++ main.c
@@ -7,6 +7,39 @@ void c3_p6() {
     }
 }
 
+void c4_p4() {
+    int x = 10;
+    int y = 1;
+    int z;
+    if (x < y) {
+        z = x;
+    } else {
+        z = y;
+    }
+    printf("%d\n", z);
+}
+
+void c4_p14() {
+    int sum = 0;
+    int i = 1;
+    while (i < 11) {
+        sum += i;
+        i++;
+    }
+    printf("%d\n%d\n", sum, i);
+}
+
+void c6_p2() {
+    int A[4][8] = {8};
+    int sum = 0;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            sum += A[i][j];
+        }
+    }
+    printf("%d\n", sum);
+}
+
 int main() {
     c3_p6();
 
